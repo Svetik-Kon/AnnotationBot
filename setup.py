a1178d7c00e4c791e5e0ee9acf6572d4eea1ad8d
Svetlana <svetiksemicvetik04@gmail.com>
2024-05-02 23:21:19 +0300

    Сгенерировано с помощью setuptools

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+# with open('requirements.txt') as f:
+#     requirements = f.read().splitlines()
 
 setup(
     name='AnnotationBot',
     version='1.0.0',
     packages=find_packages(),
-    install_requires=requirements,
+    # install_requires=requirements,
     entry_points={
         'console_scripts': [
             'my_project = main:main',  # Замените main на имя вашего основного модуля
