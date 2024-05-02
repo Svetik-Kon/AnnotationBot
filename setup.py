fd2d7eaba877d7c17445b2049e5a4fb71e5f8a06
Svetlana <svetiksemicvetik04@gmail.com>
2024-05-02 23:13:02 +0300

    Добавлен файл setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='AnnotationBot',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'my_project = main:main',  # Замените main на имя вашего основного модуля
+        ],
+    },
+)
